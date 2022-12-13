c4c1330fcf2482e73705d5aae91ae88cfad62db4
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2022-12-13 09:36:39 +0200

    linked exam component to backend service

--- unisaexamsubmission/src/app/exam/exam.component.ts
+++ unisaexamsubmission/src/app/exam/exam.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExamsetupService } from '../services/examsetup.service';
 
 @Component({
   selector: 'app-exam',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exam.component.scss']
 })
 export class ExamComponent implements OnInit {
+  exam : string =''
+  constructor(private examSetupService: ExamsetupService) { }
 
-  constructor() { }
+  availableExams: any;
 
   ngOnInit(): void {
+  this.showExams();
+
+  }
+
+
+  showExams(){
+    this.availableExams = this.examSetupService.showExams().subscribe(examsetup=>{
+      this.availableExams = examsetup;
+      console.log(this.availableExams);
+      
+    });
   }
 
 }
