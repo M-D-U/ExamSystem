3df8cdbd63dedaba3ca0cfa21f65a0ba147df130
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2022-12-13 12:21:20 +0200

    imported examservice to enable backend linkage

--- unisaexamsubmission/src/app/home/home.component.ts
+++ unisaexamsubmission/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ExamsetupService } from '../services/examsetup.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,35 +9,41 @@ export class HomeComponent implements OnInit {
 
   randomstring = '';
   user = 'OD MASEKO';
+  studentNumber = 51331403
   date = new Date();
   uploadDocument = false;
-  examSetup = {name: 'CHE1709', DateExam: new Date(20220629), Description:' Introductory Chemisty in Industrial Development', staffEmail: 'jh.nel@unisa.ac.za', ExamPaper: '../assets/exampaper/101_2020_3b.pdf'};
-  constructor() { }
+  // examSetup = {name: 'CHE1809', DateExam: new Date(20220629), Description:' Introductory Chemisty in Industrial Development', staffEmail: 'jh.nel@unisa.ac.za', ExamPaper: '../assets/exampaper/101_2020_3b.pdf'};
+  constructor( private examSetupService: ExamsetupService) { }
 
+  availableExams: any;
   ngOnInit(): void {
     // this.showDate();
     setInterval(() => {
       this.date = new Date();
     }, 1);
 
-    this.showExams();
+    // this.showExams();
+    this.todayExam();
   }
 
 
   showExams(){
-    /* this.date.getFullYear();
-    this.date.getMonth();
-    this.date.getDay();
-    const exam = this.examSetup.DateExam
-    console.log(this.date.getMonth(), 'is current month');
-    exam.getMonth()
-    if(exam.getMonth() === this.date.getMonth()){
-      window.alert('the month matches')
-    }else{
-      window.alert('no exam today')
-    } */
+    this.availableExams = this.examSetupService.showExams().subscribe(examsetup=>{
+      this.availableExams = examsetup;
+      console.log(this.availableExams);
+      
+    });
   }
 
+  // check for todays exams for the specific student
+  todayExam(){
+    /* if(this.examSetup.name ==='CHE1709'){
+      console.log('today  are  writng');
+    }else{
+      console.log('relax for today');
+      console.log('your exam is on the '+ this.examSetup.DateExam)
+    } */
+  }  
 
   upload(){
   //  this.uploadDocument = !this.uploadDocument;
