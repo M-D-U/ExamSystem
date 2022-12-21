f860f65fc91aad7dc7d30ea9e925ddb97c2695f0
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2022-12-21 22:54:21 +0200

    added time stamps to exam output migration

--- backend/database/migrations/2022_11_24_072219_create_examoutput_table.php
+++ backend/database/migrations/2022_11_24_072219_create_examoutput_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('uploadTime');
             $table->string('answerPaperPDF');
             $table->string('studentNumber')->index();
+            $table->timestamps(false);
         });
     }
 
