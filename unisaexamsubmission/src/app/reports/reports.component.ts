db7a2e17576f48f0fe919db551ba73090971d894
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-01-14 14:37:14 +0200

    total uploads and total modules pulling frm bckend

--- unisaexamsubmission/src/app/reports/reports.component.ts
+++ unisaexamsubmission/src/app/reports/reports.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ExamoutputService } from '../services/examoutput.service';
+import { ModuleinfoService } from '../services/moduleinfo.service';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  totalSubmissions : any;
+  overallTotal = 0;
+  totalNumberModules: any;
+  toalcountModules = 0
+  constructor(private examOutputServe: ExamoutputService, private moduleinfoService: ModuleinfoService) { }
 
   ngOnInit(): void {
+    this.getTotalSubmissions();
+    this.totalModules();
   }
 
+  // get the total number of exam submissions
+  getTotalSubmissions(){
+    this.examOutputServe.viewTotalExamSubmissions().subscribe((res) => {
+      this.totalSubmissions = res;
+      this.overallTotal = this.totalSubmissions.length;
+      console.log(this.overallTotal);
+      console.table(res);
+    });
+  }
+
+  totalModules(){
+    this.moduleinfoService.getTotalModules().subscribe((res) => {
+      this.totalNumberModules = res;
+      this.toalcountModules = this.totalNumberModules.length;
+      console.log(this.toalcountModules);
+      console.table(res);
+    });
+  }
 }
