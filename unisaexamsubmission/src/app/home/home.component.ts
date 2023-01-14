b19766cac6d6cc2c9c176d09507150e991941251
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-01-14 11:42:26 +0200

    pdf files can be sent via post method
    form data now can sent all the variables to the backend api

--- unisaexamsubmission/src/app/home/home.component.ts
+++ unisaexamsubmission/src/app/home/home.component.ts
@@ -1,48 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { ExamsetupService } from '../services/examsetup.service';
 import { ExamoutputService } from '../services/examoutput.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { HttpHeaders } from '@angular/common/http';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-
-  startTime = '10:23:11';
-  uploadTime = '11:23:11'
+  startTime = '11:23:11';
+  uploadTime = new Date().toTimeString();
   user = 'OD MASEKO';
-  studentNumber = 51331403
+  studentNumber: string = '51333403';
   date = new Date();
-  uploadDocument = '';
-  exam = '';
-  // examUpload: any;
-  examUpload: any;
-  // answerPaperPDF: { answerPaperPDF: string; }; 
-  examSetup = {name: 'CHE1809', DateExam: new Date(20220629), Description:' Introductory Chemisty in Industrial Development', staffEmail: 'jh.nel@unisa.ac.za', ExamPaper: '../assets/exampaper/101_2020_3b.pdf'};
-  constructor(private formBuilder: FormBuilder, private examSetupService: ExamsetupService, private examOutputServe:ExamoutputService) { }
+  examSetup = {name: 'CHE1809',DateExam: new Date(20220629),Description: ' Introductory Chemisty in Industrial Development',staffEmail: 'jh.nel@unisa.ac.za'};// ExamPaper: '../assets/exampaper/101_2020_3b.pdf' };
+  constructor(private examSetupService: ExamsetupService,private examOutputServe: ExamoutputService) {}
 
-  
-  ngOnInit(): void {
-    setInterval(() => {
-      this.date = new Date();
-    }, 1);
+  answerPaperPDF: File | any = null;
 
-    this.examUpload = this.formBuilder.group({
-      exam: ''
-    })
-    // console.log(this.studentNumber,this.startTime,this.uploadTime,this.answerPaperPDF)
+  // takes in the file input 
+  onFileSelected(e:any) {
+    this.answerPaperPDF =e.target.files[0];
+    console.log('checking to see if the file if its there', this.answerPaperPDF);
   }
 
-  uploadExam(exam:string){
-    this.examUpload = {
-      'startTime' : this.startTime,
-      'uploadTime' : this.uploadTime,
-      'answerPaperPDF': this.exam,
-      'studentNumber' : this.studentNumber
-    }; 
-    console.log(this.studentNumber +'my stuff ');
-  } 
+  // uploads the exam data to the API
+  onUpload(f: NgForm) {
+    var myFormData = new FormData();
+    const headers = new HttpHeaders();
+      headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Accept', 'application/json');
+      // add information to form data
+      myFormData.append('startTime', this.startTime);
+      myFormData.append('uploadTime', this.uploadTime);
+      myFormData.append('answerPaperPDF', this.answerPaperPDF);
+      myFormData.append('studentNumber',this.studentNumber)
+    // call the exam upload service api
+    this.examOutputServe.uploadExamination(myFormData).subscribe((res) => {
+      headers: headers//add headers to send form as multipart form
+      console.log(res);
+      console.log(myFormData);
+      if(res.status){
+        window.alert(res.message)
+      };
+    });
+    
+  }
 
- }
+  // initializes the date
+  ngOnInit(): void {
+    setInterval(() => {
+      this.date = new Date();//shows the current date and time on the application
+    }, 1);
+  }
+ 
+}
