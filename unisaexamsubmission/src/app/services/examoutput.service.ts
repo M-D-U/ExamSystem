ba4cc954d37bed42dc905847f4c99acf82fefb58
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-01-14 11:40:07 +0200

    added http headers
    headers allowed multipart file to be sent

--- unisaexamsubmission/src/app/services/examoutput.service.ts
+++ unisaexamsubmission/src/app/services/examoutput.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,17 @@ import { HttpClient } from '@angular/common/http';
 export class ExamoutputService {
 
   url :string = 'http://127.0.0.1:8000' ;
+  httpClient: any;
+  handleError: any;
+  headers: HttpHeaders | { [header: string]: string | string[]; } | undefined;
   constructor( private http:HttpClient) { }
 
+  uploadExamination(examUpload: any):Observable<any>{
+    const headers = new HttpHeaders();
+      headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Accept', 'application/json');
+   return this.http.post<any>(this.url+`/api/examoutput`,examUpload,{
+    headers: this.headers
+   });
+  }
 }
