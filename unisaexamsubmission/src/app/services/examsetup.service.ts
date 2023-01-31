23296467414a416a906ffb76e7f4e5b6b9eaba8d
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-01-31 12:04:29 +0200

    pdf upload working

--- unisaexamsubmission/src/app/services/examsetup.service.ts
+++ unisaexamsubmission/src/app/services/examsetup.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ExamsetupService {
 
   url :string = 'http://127.0.0.1:8000' ;
-  constructor( private http:HttpClient) { }
+  httpClient: any;
+  handleError: any;
+  headers: HttpHeaders | { [header: string]: string | string[]; } | undefined;
+
+  constructor( private http:HttpClient ) { }
+
+  uploadExamination(myFormData: any):Observable<any>{
+    const headers = new HttpHeaders();
+      headers.append('Content-Type', 'multipart/form-data');
+      headers.append('Accept', 'application/json');
+   return this.http.post<any>(this.url+`/api/examsetup`,myFormData,{
+    headers: this.headers
+   });
+  }
 
   showExams(){
     return this.http.get<any>(this.url+'/api/examsetups');
