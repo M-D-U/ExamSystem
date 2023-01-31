9e71261b967d2dd4ffbf776ea5e13376634fd667
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-01-31 12:15:08 +0200

    cleaned out examsetup

--- backend/app/Http/Controllers/ExamSetupController.php
+++ backend/app/Http/Controllers/ExamSetupController.php
@@ -30,7 +30,26 @@ class ExamSetupController extends Controller
     public function store(Request $request)
     {
         //
-        return examsetup::create($request->all());
+        // return examsetup::create($request->all());
+        $examsetup = new examsetup();
+        // $refString = "R";
+        // $examout->transactionID = $request->input('transactionID') . $refString . '' . random_int(000000000000, 999999999999);//.trim(chunk_split($examout->transactionID), 3); //retrieving user inputs
+        $examsetup->moduleCode = $request->input('moduleCode');  //retrieving user inputs
+        $examsetup->dateExam = $request->input('dateExam');  //retrieving user inputs
+            if($request->hasFile('examPaperPDF')){
+                $completeFileName = $request->file('examPaperPDF')->getClientOriginalName();
+                $fileNameOnly = pathinfo($completeFileName, PATHINFO_FILENAME);
+                $extension = $request->file('examPaperPDF')->getClientOriginalExtension();
+                $completePDF = str_replace('', '_', $fileNameOnly) . '_' . rand() . '_' . time() . '.' .$extension;
+                $path = $request->file('examPaperPDF')->storeAs('public/examPapers', $completePDF);
+                $examsetup->examPaperPDF = $completeFileName;
+            }
+        // $examout->studentNumber = $request->input('studentNumber');  //retrieving user inputs
+        if($examsetup->save()){
+                return ['status' => true, 'message' => 'Your exam paper is submitted successfully'];
+            }else{
+                return ['status' => false, 'message' => 'Exam paper is not saved, please try again'];
+             }
     }
 
     /**
@@ -54,28 +73,35 @@ class ExamSetupController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
+        $examsetup = new examsetup();
         if(examsetup::where('id',$id)->exists()){
-             $examsetup = examsetup::find($id);
-            $examsetup->dateExam = $request->dateExam;
-            if($request->hasFile('examPaperPDF')){
-            $completeFileName = $request->file('examPaperPDF')->getClientOriginalName();
-            //    $fileNameOnly = pathinfo($completeFileName, PATHINFO_ALL);
-            //    $extension = $request->file('examPaperPDF')->getClientOriginalExtension();
-            //    $filename = str_replace('','_',$fileNameOnly).'-'.rand().'_'.time().'.'.$extension;
-            $path = $request->file('examPaperPDF')->storeAs('public/examPapers',$completeFileName);
-            dd($path);
+            $examsetup = examsetup::findOrFail($id);
+            $examsetup->dateExam = $request->input('dateExam', '2023-01-19');
+            //$examout->uploadTime = $request->input('uploadTime');  //retrieving user inputs
+            if($request->hasFile('answerPaperPDF')){
+                $completeFileName = $request->file('answerPaperPDF')->getClientOriginalName();
+                $fileNameOnly = pathinfo($completeFileName, PATHINFO_FILENAME);
+                $extension = $request->file('answerPaperPDF')->getClientOriginalExtension();
+                $completePDF = str_replace('', '_', $fileNameOnly) . '_' . rand() . '_' . time() . '.' .$extension;
+                $path = $request->file('answerPaperPDF')->storeAs('public/answerPapersPDF', $completePDF);
+                $examsetup->answerPaperPDF = $completeFileName;
             }
-            $examsetup->moduleCode = $request->moduleCode;
-            
-            $examsetup->save();
+            // $examsetup->moduleCode = 'ICT1117';
+            $examsetup->moduleCode = $request->input('moduleCode', 'ICT1113');
+            }            
+            if($examsetup->save()){
+                return response()->json(['message' => 'all went well'], 422);//['status' => true, 'message' => 'Your exam paper is saved'];
+            }else{
+                return ['status' => false, 'message' => 'Exam paper is not saved, please try again'];
+             }
+            /* $examsetup->save(){
                 return response()->json(['message' => 'all went well'], 422);
             }else{
                 
             return response()->json([
                 "message" => "couldn't save your exam"
             ],200);
-            }
+            } */
             
     
     
