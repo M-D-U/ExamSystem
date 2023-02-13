40ee448183c16aefec0dfb0d9337a9401027d763
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-02-13 19:25:44 +0200

    database backup command file created

---
+++ backend/app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+// use Artisan;
+use Illuminate\Support\Facades\Artisan;
+
+
+class BackupDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup:database';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Backup the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Artisan::call('backup:run');
+    }
+}
