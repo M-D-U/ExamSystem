9f4318c8706d979066a54e20ffd2246ff232f1e7
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-02-13 19:27:14 +0200

    Jobs to try and automate the backup process for the database

---
+++ backend/app/Jobs/BackupDatabase.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\DbDumper\Databases\MySql;
+
+class BackupDatabase implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        MySql::create()
+            ->setDbName(config('database.connections.mysql.database'))
+            ->setUserName(config('database.connections.mysql.username'))
+            ->setPassword(config('database.connections.mysql.password'))
+            ->dumpToFile(storage_path('backups/backup.sql'));
+    }
+}
