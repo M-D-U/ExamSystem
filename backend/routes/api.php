b6bc25db1dda3b02589bd648a8e1ee8525029e28
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-02-15 08:40:31 +0200

    added routes to weekly and daily reports

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Resources\ExamSetupResource;
 use App\Http\Controllers\ExamOutputController;
 use App\Http\Resources\ExamOutputResource;
 use App\Http\Resources\ModuleInfoResource;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -45,13 +46,22 @@ Route::get('/examsetup/{id}',function($id){
     return new ExamSetupResource(examsetup::findorFail($id));
 });
 
-// exam output routes
+/* exam output routes */
+// stores the exam paper the student submits
 Route::post('/examoutput',[ExamOutputController::class,'store']);
 
+// get the total number of exam submissions
 Route::get('/examoutputs',function(){
     return ExamOutputResource::collection(examoutput::all());
 });
+// get the total number of exam submissions for today
+Route::get('/examoutputs/total-files-today', [ExamOutputController::class, 'getDailyReport']);
 
+
+Route::get('/examoutputs/total-files-this-week', [ExamOutputController::class, 'getTotalFilesThisWeek']);
+
+
+/* authentication for users */
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
