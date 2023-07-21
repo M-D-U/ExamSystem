92c9c572fdf82a26d763905fff4086f17c4ea233
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-07-21 12:00:54 +0200

    backup schedule

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 
-        $schedule->command('backup:database')->everyMinute();
+        $schedule->command('backup:database')->everyFiveMinutes();
         // $schedule->job(new \App\Jobs\BackupDatabase)->daily();
         // $schedule->command('backup:run')->everyMinute();
         // $schedule->command('inspire')->everyMinute();
